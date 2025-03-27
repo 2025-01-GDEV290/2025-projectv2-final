f4bef7b08ca01c774293e15f4542ce4cc0e707de
Lmack71 <143634405+Lmack71@users.noreply.github.com>
2025-03-27 12:50:32 -0400

    Cut Scene
    Bang

--- Assets/scripts/Cutscene.cs
+++ Assets/scripts/Cutscene.cs
@@ -64,7 +64,7 @@ public class Cutscene : MonoBehaviour
         foreach (char letter in message.ToCharArray())
         {
             textUI.text += letter;
-            yield return new WaitForSeconds(0.1f);  
+            yield return new WaitForSeconds(0.03f);  
         }
     }
 
