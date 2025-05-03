1deb071b019d2b17f8f908d07820403163f8d835
Lufer115 <vargas.lufer1@gmail.com>
2025-05-03 17:41:28 -0400

    .

--- Assets/scripts/UI/StartJourneyPopup.cs
+++ Assets/scripts/UI/StartJourneyPopup.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartJourneyPopup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject popupUI;
+    public GameObject playerObject;
+
+    private bool hasTriggered = false;
+
+    private void Start()
     {
-        
+        popupUI.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (!hasTriggered && other.CompareTag("Player"))
+        {
+            popupUI.SetActive(true);
+            Time.timeScale = 0f;
+            hasTriggered = true;
+
+            // Disable movement and camera scripts
+            playerObject.GetComponent<PlayerController>().enabled = false;
+            playerObject.GetComponentInChildren<CameraController>().enabled = false; // <-- NEW
+        }
+    }
+
+    public void OnOkButtonPressed()
+    {
+        popupUI.SetActive(false);
+        Time.timeScale = 1f;
+
+        // Enable movement and camera scripts
+        playerObject.GetComponent<PlayerController>().enabled = true;
+        playerObject.GetComponentInChildren<CameraController>().enabled = true; // <-- NEW
     }
 }
